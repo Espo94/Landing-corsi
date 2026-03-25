e3599ce962cf4a87d8d5d861cd1582b26e94f6cb
Luca <92849798+Espo94@users.noreply.github.com>
2026-03-25 19:30:23 +0000

    nuovo logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ export default function Header() {
   return (
     <header className="header">
       <div className="header-inner">
-        <img className="logo" src="/logo.png" alt="Associazione C.E.I.O" />
+        <img className="logo" src="/logo.jpeg" alt="Associazione C.E.I.O" />
         <nav className="nav">
           <a href="#programs">Programmi</a>
           <a href="#how">Come funziona</a>
